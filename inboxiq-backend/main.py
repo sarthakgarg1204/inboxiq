ec1217651adda2cba1a9f3a3be201d631401c002
sarthakgarg1204 <sarthakgarg7124@gmail.com>
2025-06-08 12:37:27 +0530

    Updated Predict.py

--- inboxiq-backend/main.py
+++ inboxiq-backend/main.py
@@ -5,12 +5,18 @@ from api.predict import router as predict_router
 
 app = FastAPI()
 
-frontend_url = os.getenv("FRONTEND_URL", "*")
-print("Using FRONTEND_URL for CORS:", frontend_url)
+# frontend_url = os.getenv("FRONTEND_URL", "*")
+# print("Using FRONTEND_URL for CORS:", frontend_url)
+
+origins = [
+    "https://inboxiq-one.vercel.app",
+    # add more origins if needed
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url],
+    # allow_origins=[frontend_url],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
